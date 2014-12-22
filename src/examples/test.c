15df6500e00ce83b11bed186f51f6a1bda3b45c9
Max Grossman <max@delphix.com>
2014-12-21 18:29:47 -0800

    simple source code examples

--- src/examples/test.c
+++ src/examples/test.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
 
+static int foo(int a);
+
+static int bar(int b) {
+    return foo(b + 2);
+}
+
+static int foo(int a) {
+    return bar(a + 1);
+}
+
 int main(int argc, char **argv) {
     int i;
     int N = 10;
-    int *a = (int *)malloc(sizeof(int) * N);
+    int * arr1 = (int *)malloc(sizeof(int) * N);
+    int * arr2 = (int *)malloc(sizeof(int) * N);
+    // int **alias1 = &arr1;
+    int *alias2 = arr1 + 4;
+    int *alias3 = alias2 + 2;
 
     for (i = 0; i < N; i++) {
-        a[i] = i;
+        alias2[i] = foo(i);
+        arr1[i] = foo(i + 1);
+        *alias3 = foo(i + 1);
     }
 
-    free(a);
+    free(arr1);
+    free(arr2);
     return 0;
 }
