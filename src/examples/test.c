d67a754cfce34355c514d96bdb9b0a7ad32128fc
Max Grossman <jmaxg3@gmail.com>
2014-12-22 22:03:17 -0800

    preliminary implementation

--- src/examples/test.c
+++ src/examples/test.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 
+int bar(int *callee_ptr2) {
+    return *callee_ptr2 + 2;
+}
+
+int foo(int *callee_ptr) {
+    return *callee_ptr + 1;
+}
+
 int main(int argc, char **argv) {
-    int *a = NULL;
-    float *b = NULL;
+    int caller_var = 3;
+    int caller_var2 = 4;
+    foo(&caller_var);
+    bar(&caller_var2);
     return 0;
 }
