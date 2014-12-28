8347bc59c4d7230801df64c9522a741b732afc72
Max Grossman <jmaxg3@gmail.com>
2014-12-28 15:18:10 -0800

    update simple test to dump output

--- src/examples/simple_stencil.cpp
+++ src/examples/simple_stencil.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "checkpoint.h"
+
 int main(int argc, char **argv) {
     int i, iter;
     int N = 1024;
-    int iters = 100;
+    int niters = 10000;
 
     int *curr = (int *)malloc(N * sizeof(int));
     int *next = (int *)malloc(N * sizeof(int));
@@ -13,14 +15,24 @@ int main(int argc, char **argv) {
     }
 
     for (iter = 0; iter < niters; iter++) {
+        printf("%d / %d\n", iter + 1, niters);
         for (i = 1; i < N - 1; i++) {
             next[i] = (curr[i - 1] + curr[i] + curr[i + 1]) / 3;
         }
         int *tmp = curr;
         curr = next;
         next = tmp;
+
+        checkpoint();
     }
 
-    free(arr);
+    FILE *fp = fopen("dump.out", "w");
+    for (i = 0; i < N; i++) {
+        fprintf(fp, "%d\n", curr[i]);
+    }
+    fclose(fp);
+
+    free(curr);
+    free(next);
     return 0;
 }
