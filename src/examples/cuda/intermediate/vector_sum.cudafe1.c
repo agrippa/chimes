0a18dc189d0a852299999ead741ed556cb486beb
Max Grossman <jmaxg3@gmail.com>
2014-12-29 17:00:32 -0600

    fix with call to checkpoint

--- src/examples/cuda/intermediate/vector_sum.cudafe1.c
+++ src/examples/cuda/intermediate/vector_sum.cudafe1.c
@@ -1,11 +1,11 @@
 # 1 "../vector_sum.cu"
-# 35 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/exception" 3
+# 35 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/exception" 3
 #pragma GCC visibility push ( default )
-# 144 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/exception" 3
+# 149 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/exception" 3
 #pragma GCC visibility pop
-# 42 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/new" 3
+# 42 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/new" 3
 #pragma GCC visibility push ( default )
-# 110 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/new" 3
+# 120 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/new" 3
 #pragma GCC visibility pop
 # 1388 "/opt/apps/cuda/6.0.37/bin/..//include/driver_types.h"
 struct CUstream_st;
@@ -36,51 +36,53 @@ _SVID_,
 _XOPEN_,
 _POSIX_,
 _ISOC_};
-# 124 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 128 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt9__is_voidIvEUt_E { _ZNSt9__is_voidIvE7__valueE = 1};
-# 144 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 148 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIbEUt_E { _ZNSt12__is_integerIbE7__valueE = 1};
-# 151 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 155 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIcEUt_E { _ZNSt12__is_integerIcE7__valueE = 1};
-# 158 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 162 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIaEUt_E { _ZNSt12__is_integerIaE7__valueE = 1};
-# 165 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 169 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIhEUt_E { _ZNSt12__is_integerIhE7__valueE = 1};
-# 173 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 177 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIwEUt_E { _ZNSt12__is_integerIwE7__valueE = 1};
-# 197 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 201 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIsEUt_E { _ZNSt12__is_integerIsE7__valueE = 1};
-# 204 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 208 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerItEUt_E { _ZNSt12__is_integerItE7__valueE = 1};
-# 211 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 215 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIiEUt_E { _ZNSt12__is_integerIiE7__valueE = 1};
-# 218 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 222 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIjEUt_E { _ZNSt12__is_integerIjE7__valueE = 1};
-# 225 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 229 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIlEUt_E { _ZNSt12__is_integerIlE7__valueE = 1};
-# 232 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 236 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerImEUt_E { _ZNSt12__is_integerImE7__valueE = 1};
-# 239 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 243 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIxEUt_E { _ZNSt12__is_integerIxE7__valueE = 1};
-# 246 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 250 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt12__is_integerIyEUt_E { _ZNSt12__is_integerIyE7__valueE = 1};
-# 264 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 268 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt13__is_floatingIfEUt_E { _ZNSt13__is_floatingIfE7__valueE = 1};
-# 271 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 275 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt13__is_floatingIdEUt_E { _ZNSt13__is_floatingIdE7__valueE = 1};
-# 278 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 282 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt13__is_floatingIeEUt_E { _ZNSt13__is_floatingIeE7__valueE = 1};
-# 354 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 358 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt9__is_charIcEUt_E { _ZNSt9__is_charIcE7__valueE = 1};
-# 362 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 366 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt9__is_charIwEUt_E { _ZNSt9__is_charIwE7__valueE = 1};
-# 377 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 381 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt9__is_byteIcEUt_E { _ZNSt9__is_byteIcE7__valueE = 1};
-# 384 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 388 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt9__is_byteIaEUt_E { _ZNSt9__is_byteIaE7__valueE = 1};
-# 391 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/../../../../include/c++/4.4.7/bits/cpp_type_traits.h" 3
+# 395 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
 enum _ZNSt9__is_byteIhEUt_E { _ZNSt9__is_byteIhE7__valueE = 1};
-# 211 "/usr/lib/gcc/x86_64-redhat-linux/4.4.7/include/stddef.h" 3
+# 138 "/opt/apps/gcc/4.8.2/include/c++/4.8.2/bits/cpp_type_traits.h" 3
+enum _ZNSt12__is_integerIeEUt_E { _ZNSt12__is_integerIeE7__valueE}; enum _ZNSt12__is_integerIdEUt_E { _ZNSt12__is_integerIdE7__valueE}; enum _ZNSt12__is_integerIfEUt_E { _ZNSt12__is_integerIfE7__valueE};
+# 212 "/opt/apps/gcc/4.8.2/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/include/stddef.h" 3
 typedef unsigned long size_t;
 #include "crt/host_runtime.h"
 # 49 "/usr/include/stdio.h" 3
@@ -98,7 +100,9 @@ extern enum cudaError cudaMemcpy(void *, const void *, size_t, enum cudaMemcpyKi
 extern int fclose(FILE *);
 # 271 "/usr/include/stdio.h" 3
 extern FILE *fopen(const char *__restrict__, const char *__restrict__);
-# 19 "../vector_sum.cu"
+# 4 "/scratch/jmg3/num-debug/src/libnumdebug/checkpoint.h"
+extern void _Z10checkpointv(void);
+# 20 "../vector_sum.cu"
 extern int main(int, char **);
 extern int __cudaSetupArgSimple();
 extern int __cudaLaunch();
@@ -107,72 +111,70 @@ extern void __nv_save_fatbinhandle_for_managed_rt();
 extern int __cudaRegisterEntry();
 extern int __cudaRegisterBinary();
 static void __sti___18_vector_sum_cpp1_ii_36d5b762(void) __attribute__((__constructor__));
-# 19 "../vector_sum.cu"
+# 20 "../vector_sum.cu"
 int main( int argc,  char **argv) {  struct dim3 __T20;
  unsigned __T21;
  struct dim3 __T22;
  unsigned __T23;
-# 21 "../vector_sum.cu"
- int __cuda_local_var_33982_9_non_const_i;
- int __cuda_local_var_33983_9_non_const_N;
-
- int *__cuda_local_var_33985_10_non_const_h_A;
-# 24 "../vector_sum.cu"
- int *__cuda_local_var_33985_16_non_const_h_B;
-# 24 "../vector_sum.cu"
- int *__cuda_local_var_33985_22_non_const_h_C;
- int *__cuda_local_var_33986_10_non_const_d_A;
-# 25 "../vector_sum.cu"
- int *__cuda_local_var_33986_16_non_const_d_B;
-# 25 "../vector_sum.cu"
- int *__cuda_local_var_33986_22_non_const_d_C;
-# 43 "../vector_sum.cu"
- int __cuda_local_var_34004_9_non_const_threads_per_block;
- int __cuda_local_var_34005_9_non_const_blocks_per_grid;
-
-
-
-
-
- FILE *__cuda_local_var_34011_11_non_const_fp;
 # 22 "../vector_sum.cu"
-__cuda_local_var_33983_9_non_const_N = 1048576;
+ int __cuda_local_var_34056_9_non_const_i;
+ int __cuda_local_var_34057_9_non_const_N;
+
+ int *__cuda_local_var_34059_10_non_const_h_A;
+# 25 "../vector_sum.cu"
+ int *__cuda_local_var_34059_16_non_const_h_B;
+# 25 "../vector_sum.cu"
+ int *__cuda_local_var_34059_22_non_const_h_C;
+ int *__cuda_local_var_34060_10_non_const_d_A;
+# 26 "../vector_sum.cu"
+ int *__cuda_local_var_34060_16_non_const_d_B;
+# 26 "../vector_sum.cu"
+ int *__cuda_local_var_34060_22_non_const_d_C;
+# 44 "../vector_sum.cu"
+ int __cuda_local_var_34078_9_non_const_threads_per_block;
+ int __cuda_local_var_34079_9_non_const_blocks_per_grid;
+# 53 "../vector_sum.cu"
+ FILE *__cuda_local_var_34087_11_non_const_fp;
+# 23 "../vector_sum.cu"
+__cuda_local_var_34057_9_non_const_N = 1048576;
 
 
 
 
-__cuda_local_var_33985_10_non_const_h_A = ((int *)(malloc((4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)))));
-__cuda_local_var_33985_16_non_const_h_B = ((int *)(malloc((4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)))));
-__cuda_local_var_33985_22_non_const_h_C = ((int *)(malloc((4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)))));
+__cuda_local_var_34059_10_non_const_h_A = ((int *)(malloc((4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)))));
+__cuda_local_var_34059_16_non_const_h_B = ((int *)(malloc((4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)))));
+__cuda_local_var_34059_22_non_const_h_C = ((int *)(malloc((4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)))));
 
-cudaMalloc(((void **)(&__cuda_local_var_33986_10_non_const_d_A)), (4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)));
-cudaMalloc(((void **)(&__cuda_local_var_33986_16_non_const_d_B)), (4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)));
-cudaMalloc(((void **)(&__cuda_local_var_33986_22_non_const_d_C)), (4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)));
+cudaMalloc(((void **)(&__cuda_local_var_34060_10_non_const_d_A)), (4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)));
+cudaMalloc(((void **)(&__cuda_local_var_34060_16_non_const_d_B)), (4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)));
+cudaMalloc(((void **)(&__cuda_local_var_34060_22_non_const_d_C)), (4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)));
 
-for (__cuda_local_var_33982_9_non_const_i = 0; (__cuda_local_var_33982_9_non_const_i < __cuda_local_var_33983_9_non_const_N); __cuda_local_var_33982_9_non_const_i++) {
-(__cuda_local_var_33985_10_non_const_h_A[__cuda_local_var_33982_9_non_const_i]) = __cuda_local_var_33982_9_non_const_i;
-(__cuda_local_var_33985_16_non_const_h_B[__cuda_local_var_33982_9_non_const_i]) = (2 * __cuda_local_var_33982_9_non_const_i);
+for (__cuda_local_var_34056_9_non_const_i = 0; (__cuda_local_var_34056_9_non_const_i < __cuda_local_var_34057_9_non_const_N); __cuda_local_var_34056_9_non_const_i++) {
+(__cuda_local_var_34059_10_non_const_h_A[__cuda_local_var_34056_9_non_const_i]) = __cuda_local_var_34056_9_non_const_i;
+(__cuda_local_var_34059_16_non_const_h_B[__cuda_local_var_34056_9_non_const_i]) = (2 * __cuda_local_var_34056_9_non_const_i);
 }
 
-cudaMemcpy(((void *)__cuda_local_var_33986_10_non_const_d_A), ((const void *)__cuda_local_var_33985_10_non_const_h_A), (4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)), cudaMemcpyHostToDevice);
-cudaMemcpy(((void *)__cuda_local_var_33986_16_non_const_d_B), ((const void *)__cuda_local_var_33985_16_non_const_h_B), (4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)), cudaMemcpyHostToDevice);
+cudaMemcpy(((void *)__cuda_local_var_34060_10_non_const_d_A), ((const void *)__cuda_local_var_34059_10_non_const_h_A), (4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)), cudaMemcpyHostToDevice);
+cudaMemcpy(((void *)__cuda_local_var_34060_16_non_const_d_B), ((const void *)__cuda_local_var_34059_16_non_const_h_B), (4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)), cudaMemcpyHostToDevice);
 
-__cuda_local_var_34004_9_non_const_threads_per_block = 128;
-__cuda_local_var_34005_9_non_const_blocks_per_grid = (((__cuda_local_var_33983_9_non_const_N + __cuda_local_var_34004_9_non_const_threads_per_block) - 1) / __cuda_local_var_34004_9_non_const_threads_per_block);
+__cuda_local_var_34078_9_non_const_threads_per_block = 128;
+__cuda_local_var_34079_9_non_const_blocks_per_grid = (((__cuda_local_var_34057_9_non_const_N + __cuda_local_var_34078_9_non_const_threads_per_block) - 1) / __cuda_local_var_34078_9_non_const_threads_per_block);
 
-(cudaConfigureCall((((void)((__T21 = ((unsigned)__cuda_local_var_34005_9_non_const_blocks_per_grid)) , (void)((((__T20.x) = __T21) , (void)((__T20.y) = 1U)) , ((__T20.z) = 1U)))) , __T20), (((void)((__T23 = ((unsigned)__cuda_local_var_34004_9_non_const_threads_per_block)) , (void)((((__T22.x) = __T23) , (void)((__T22.y) = 1U)) , ((__T22.z) = 1U)))) , __T22), 0UL, ((struct CUstream_st *)0LL))) ? ((void)0) : (__device_stub__Z6kernelPiS_S_i(__cuda_local_var_33986_10_non_const_d_A, __cuda_local_var_33986_16_non_const_d_B, __cuda_local_var_33986_22_non_const_d_C, __cuda_local_var_33983_9_non_const_N));
+(cudaConfigureCall((((void)((__T21 = ((unsigned)__cuda_local_var_34079_9_non_const_blocks_per_grid)) , (void)((((__T20.x) = __T21) , (void)((__T20.y) = 1U)) , ((__T20.z) = 1U)))) , __T20), (((void)((__T23 = ((unsigned)__cuda_local_var_34078_9_non_const_threads_per_block)) , (void)((((__T22.x) = __T23) , (void)((__T22.y) = 1U)) , ((__T22.z) = 1U)))) , __T22), 0UL, ((struct CUstream_st *)0LL))) ? ((void)0) : (__device_stub__Z6kernelPiS_S_i(__cuda_local_var_34060_10_non_const_d_A, __cuda_local_var_34060_16_non_const_d_B, __cuda_local_var_34060_22_non_const_d_C, __cuda_local_var_34057_9_non_const_N));
 
-cudaMemcpy(((void *)__cuda_local_var_33985_22_non_const_h_C), ((const void *)__cuda_local_var_33986_22_non_const_d_C), (4UL * ((unsigned long)__cuda_local_var_33983_9_non_const_N)), cudaMemcpyDeviceToHost);
+cudaMemcpy(((void *)__cuda_local_var_34059_22_non_const_h_C), ((const void *)__cuda_local_var_34060_22_non_const_d_C), (4UL * ((unsigned long)__cuda_local_var_34057_9_non_const_N)), cudaMemcpyDeviceToHost);
 
-__cuda_local_var_34011_11_non_const_fp = (fopen(((const char *)"dump.out"), ((const char *)"w")));
-for (__cuda_local_var_33982_9_non_const_i = 0; (__cuda_local_var_33982_9_non_const_i < __cuda_local_var_33983_9_non_const_N); __cuda_local_var_33982_9_non_const_i++) {
-fprintf(__cuda_local_var_34011_11_non_const_fp, ((const char *)"%d\n"), (__cuda_local_var_33985_22_non_const_h_C[__cuda_local_var_33982_9_non_const_i]));
+_Z10checkpointv();
+
+__cuda_local_var_34087_11_non_const_fp = (fopen(((const char *)"dump.out"), ((const char *)"w")));
+for (__cuda_local_var_34056_9_non_const_i = 0; (__cuda_local_var_34056_9_non_const_i < __cuda_local_var_34057_9_non_const_N); __cuda_local_var_34056_9_non_const_i++) {
+fprintf(__cuda_local_var_34087_11_non_const_fp, ((const char *)"%d\n"), (__cuda_local_var_34059_22_non_const_h_C[__cuda_local_var_34056_9_non_const_i]));
 }
-fclose(__cuda_local_var_34011_11_non_const_fp);
+fclose(__cuda_local_var_34087_11_non_const_fp);
 
-cudaFree(((void *)__cuda_local_var_33986_10_non_const_d_A));
-cudaFree(((void *)__cuda_local_var_33986_16_non_const_d_B));
-cudaFree(((void *)__cuda_local_var_33986_22_non_const_d_C));
+cudaFree(((void *)__cuda_local_var_34060_10_non_const_d_A));
+cudaFree(((void *)__cuda_local_var_34060_16_non_const_d_B));
+cudaFree(((void *)__cuda_local_var_34060_22_non_const_d_C));
 
 return 0;
 }
