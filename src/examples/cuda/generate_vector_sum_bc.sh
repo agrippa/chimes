709bdace8358adb58e3d84ff8d119ce6557b4a85
Max Grossman <jmaxg3@gmail.com>
2015-01-04 00:28:07 -0600

    update from cuda

--- src/examples/cuda/generate_vector_sum_bc.sh
+++ src/examples/cuda/generate_vector_sum_bc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 CLANG=${LLVM_INSTALL}/Debug+Asserts/bin/clang
 
-$CLANG -I${CUDA_HOME}/include -I${NUM_DEBUG_HOME}/src/libnumdebug -S -emit-llvm intermediate/vector_sum.cudafe1.cpp -o vector_sum.cudafe1.bc -g
+$CLANG -I${CUDA_HOME}/include -I${NUM_DEBUG_HOME}/src/libnumdebug -S \
+        -emit-llvm intermediate/vector_sum.cudafe1.cpp \
+        -o vector_sum.cudafe1.bc -g
