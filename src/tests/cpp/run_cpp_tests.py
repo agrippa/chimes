daaa5f4e012d5ecedf7b648ec13dcd998e4ec4c8
Max Grossman <max@delphix.com>
2015-01-05 11:00:53 -0800

    start writing up tests

---
+++ src/tests/cpp/run_cpp_tests.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from subprocess import Popen, PIPE
+
+tests = [('simple_stencil.cpp', 'simple_stencil.register.cpp')]
+
+NUM_DEBUG_HOME = os.environ['NUM_DEBUG_HOME']
+COMPILE_SCRIPT = NUM_DEBUG_HOME + '/src/preprocessing/compile_cpp.sh'
+EXAMPLES_DIR = NUM_DEBUG_HOME + '/src/examples'
+CPP_EXAMPLES_DIR = EXAMPLES_DIR + '/cpp'
+CPP_TEST_DIR = NUM_DEBUG_HOME + '/src/tests/cpp'
+
+def run_cmd(cmd):
+    cmd = cmd.split()
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+    p.wait()
+    result = (p.stdout.read(), p.stderr.read())
+
+    if p.returncode != 0:
+        print 'Error running "' + ' '.join(cmd) + '"'
+        print
+        print result[1]
+        sys.exit(1)
+    return result
+
+for t in tests:
+    input_file = t[0]
+    compare_file = t[1]
+
+    compile_cmd = COMPILE_SCRIPT + ' -k -i ' + CPP_EXAMPLES_DIR + '/' + input_file
+    stdout, stderr = run_cmd(compile_cmd)
+    lines = stdout.split('\n')
+    transformed = lines[len(lines) - 2].strip()
+
+    stdout, stderr = run_cmd('diff ' + CPP_TEST_DIR + '/' + compare_file + ' ' + transformed)
+
+    if len(stdout.strip()) != 0:
+        print 'Mismatch in test for ' + input_file
+        print stdout
+        sys.exit(1)
+    else:
+        folder = transformed[0:transformed.rfind('/')]
+        run_cmd('rm -rf ' + folder)
+        run_cmd('rm a.out')
