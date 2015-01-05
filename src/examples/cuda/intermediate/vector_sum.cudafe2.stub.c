135d5ff136d48f8b67266b1ea659b8bfbd993ee3
Max Grossman <jmaxg3@gmail.com>
2015-01-05 00:18:21 -0600

    clean up files

--- src/examples/cuda/intermediate/vector_sum.cudafe2.stub.c
+++
@@ -1,3 +0,0 @@
-#define __NV_CUBIN_HANDLE_STORAGE__ static
-#include "crt/host_runtime.h"
-#include "vector_sum.fatbin.c"
---
+++ src/preprocessing/compile_helper.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import os
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 6:
+        print('usage: compile_helper.py cmd-file env-file pre-cmds-file '
+                'post-cmds-file cpp-file')
+        sys.exit(1)
+
+    cmd_file = sys.argv[1]
+    env_file = sys.argv[2]
+    pre_cmd_file = sys.argv[3]
+    post_cmd_file = sys.argv[4]
+    cpp_file = sys.argv[5]
+
+    env_file_fp = open(env_file, 'w')
+    pre_cmd_file_fp = open(pre_cmd_file, 'w')
+    post_cmd_file_fp = open(post_cmd_file, 'w')
+    cpp_file_fp = open(cpp_file, 'w')
+
+    env = True
+    pre = False
+    post = False
+    include_inserted = False
+    cmd_file_fp = open(cmd_file, 'r')
+
+    for line in cmd_file_fp:
+        if line.find('#$ ') == 0:
+            line = line[3:]
+
+            if line.find('rm') != 0:
+                if env:
+                    assert env and not pre and not post
+                    line = '='.join([t.strip() for t in line.split('=')])
+                    env_file_fp.write(line + '\n')
+                elif pre:
+                    assert not env and pre and not post
+                    pre_cmd_file_fp.write(line)
+                else:
+                    assert not env and not pre and post
+                    if not include_inserted and line.find('gcc') == 0:
+                        line = line[3:]
+                        line = 'gcc -include${NUM_DEBUG_HOME}/src/libnumdebug/libnumdebug.h ' + line
+                        include_inserted = True
+                    post_cmd_file_fp.write(line)
+
+            if line.find('PTXAS_FLAGS') == 0:
+                assert env and not pre and not post
+                env = False
+                pre = True
+            elif line.find('cudafe++') == 0:
+                assert not env and pre and not post
+                pre = False
+                post = True
+
+                cudafe_file = None
+                tokens = line.split()
+                for i in range(len(tokens)):
+                    t = tokens[i]
+                    if t == '--gen_c_file_name':
+                        cudafe_file = tokens[i + 1]
+
+                assert cudafe_file is not None
+                cudafe_file = cudafe_file[1:]
+                cudafe_file = cudafe_file[:len(cudafe_file) - 1]
+                cpp_file_fp.write(cudafe_file)
+
+    pre_cmd_file_fp.close()
+    post_cmd_file_fp.close()
+    cpp_file_fp.close()
+
+    cmd_file_fp.close()
