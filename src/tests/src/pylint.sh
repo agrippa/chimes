09201b53318507591ec1743b5712b108c131ddfc
Max Grossman <jmaxg3@gmail.com>
2015-01-27 10:04:24 -0800

    extend pylint to run on all files

--- src/tests/src/pylint.sh
+++ src/tests/src/pylint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 if [[ $# != 1 ]]; then
-    echo usage: pylint.sh file.py
+    echo 'usage: pylint.sh [file.py|all]'
     exit 1
 fi
 
-pylint --disable=too-few-public-methods,too-many-arguments,too-many-locals \
-                 --reports=n $1
+PYLINT=pylint
+PYLINT_ARGS="--reports=n --disable=too-few-public-methods,too-many-arguments,too-many-locals"
+
+if [[ "$1" == "all" ]]; then
+    for f in $(ls *.py); do
+        echo ===== $f =====
+        $PYLINT $PYLINT_ARGS $f
+        echo
+    done
+else
+    $PYLINT $PYLINT_ARGS $1
+fi
