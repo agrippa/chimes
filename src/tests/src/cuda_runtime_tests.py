17c38f33ab99eadd54647702df91db49d2d8151d
Max Grossman <jmaxg3@gmail.com>
2015-02-02 17:02:35 -0800

    fix some tests

--- src/tests/src/cuda_runtime_tests.py
+++ src/tests/src/cuda_runtime_tests.py
@@ -6,9 +6,9 @@ import sys
 from common import RuntimeTest, parse_argv, NUM_DEBUG_HOME, run_runtime_test, \
          cleanup_runtime_files
 
-BASIC = RuntimeTest('Basic', 'basic.cu', 0, 1)
-KERNEL = RuntimeTest('Kernel', 'kernel.cu', 0, 10)
-INDIRECTION = RuntimeTest('Indirection', 'indirection.cu', 0, 3)
+BASIC = RuntimeTest('Basic', ['basic.cu'], 0, 1)
+KERNEL = RuntimeTest('Kernel', ['kernel.cu'], 0, 10)
+INDIRECTION = RuntimeTest('Indirection', ['indirection.cu'], 0, 3)
 TESTS = [BASIC, KERNEL, INDIRECTION]
 
 COMPILE_SCRIPT = NUM_DEBUG_HOME + '/src/preprocessing/compile_cuda.sh'
