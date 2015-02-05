e32912d358b8b31fcd285bbb9a7412fb209ede9d
Max Grossman <jmaxg3@gmail.com>
2015-02-05 10:42:15 -0800

    fix some memory leaks in libnumdebug

--- src/examples/cpp/compile_iso2d.sh
+++ src/examples/cpp/compile_iso2d.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ../../preprocessing/compile_cpp.sh -i ./iso2d.cpp -i lib/common.cpp \
-        -i lib/common2d.cpp -I ./include/ -L ./lib/ -l common2d -o iso2d
+        -i lib/common2d.cpp -I ./include/ -L ./lib/ -l common2d -o iso2d -k
