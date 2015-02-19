12535a51c975aa7fc6f067272dde8c394f61f312
Max Grossman <jmaxg3@gmail.com>
2015-02-18 18:33:20 -0800

    finish work on making API thread-safe

--- src/libnumdebug/heap_allocation.h
+++ src/libnumdebug/heap_allocation.h
@@ -11,7 +11,6 @@ class heap_allocation {
         void *address;
         size_t size;
         size_t alias_group;
-        uint64_t seq;
 
         int elem_is_ptr;
         int elem_is_struct;
@@ -23,23 +22,22 @@ class heap_allocation {
         int is_cuda_alloc;
 
     public:
-        heap_allocation() : address(NULL), size(0), alias_group(0), seq(0),
+        heap_allocation() : address(NULL), size(0), alias_group(0),
                 elem_is_ptr(0), elem_is_struct(0), elem_size(0),
                 tmp_buffer(NULL), is_cuda_alloc(0) { }
 
         heap_allocation(void *set_address, size_t set_size,
-                size_t set_alias_group, uint64_t set_seq,
-                int set_is_cuda_alloc, int set_elem_is_ptr,
-                int set_elem_is_struct) : address(set_address), size(set_size),
-                alias_group(set_alias_group), seq(set_seq),
-                elem_is_ptr(set_elem_is_ptr),
-                elem_is_struct(set_elem_is_struct), elem_size(0),
-                tmp_buffer(NULL), is_cuda_alloc(set_is_cuda_alloc) { }
+                size_t set_alias_group, int set_is_cuda_alloc,
+                int set_elem_is_ptr, int set_elem_is_struct) :
+            address(set_address), size(set_size),
+            alias_group(set_alias_group),
+            elem_is_ptr(set_elem_is_ptr),
+            elem_is_struct(set_elem_is_struct), elem_size(0),
+            tmp_buffer(NULL), is_cuda_alloc(set_is_cuda_alloc) { }
 
         // Getters
         size_t get_alias_group() { return alias_group; }
         void *get_address() { return address; }
-        int get_seq() { return seq; }
         int check_elem_is_ptr() { return elem_is_ptr; }
         int check_elem_is_struct() { return elem_is_struct; }
         int get_elem_size() {
