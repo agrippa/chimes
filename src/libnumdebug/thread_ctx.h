9742caf69f650c37981db10f9cdc210af29863a0
Max Grossman <jmaxg3@gmail.com>
2015-02-18 16:10:33 -0800

    fix trace maintenance

--- src/libnumdebug/thread_ctx.h
+++ src/libnumdebug/thread_ctx.h
@@ -7,6 +7,7 @@
 
 #include "stack_frame.h"
 #include "heap_allocation.h"
+#include "numdebug_stack.h"
 
 class thread_ctx {
     public:
@@ -49,6 +50,18 @@ class thread_ctx {
         void set_calling_label(int label) { calling_label = label; }
         int get_calling_label() { return calling_label; }
 
+        numdebug_stack &get_stack_tracker() { return stack_tracker; }
+
+        void push_return_alias() { return_aliases.push_back(return_alias); }
+        size_t pop_return_alias() {
+            size_t result = return_aliases.back();
+            return_aliases.pop_back();
+            return result;
+        }
+        void set_return_alias(size_t set_return_alias) {
+            return_alias = set_return_alias;
+        }
+
     private:
         pthread_t pthread;
         vector<void *> parent_vars;
@@ -57,6 +70,15 @@ class thread_ctx {
         int stack_nesting;
         set<size_t> changed_groups;
         int calling_label;
+
+        /*
+         * During normal execution, has every function call and parallel region
+         * entry recorded in it so that these transitions can be replayed later.
+         */
+        numdebug_stack stack_tracker;
+
+        size_t return_alias;
+        vector<size_t> return_aliases;
 };
 
 #endif
