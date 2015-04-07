d475195e694ab1e884a0e496665aae21083b38ed
Max Grossman <jmaxg3@gmail.com>
2015-04-07 09:06:58 -0500

    separate mac frontend results out to separate folder

--- src/tests/src/omp_frontend_tests.py
+++ src/tests/src/omp_frontend_tests.py
@@ -5,11 +5,13 @@ transformation features of chimes.
 import os
 import sys
 from common import run_frontend_test, parse_argv, \
-                   CHIMES_HOME, construct_simple_frontend_test, find_file
+                   CHIMES_HOME, construct_simple_frontend_test, find_file, \
+                   get_platform_directory
 
 COMPILE_SCRIPT = CHIMES_HOME + '/src/preprocessing/compile_cpp.sh'
 OMP_EXAMPLES_DIR = CHIMES_HOME + '/src/examples/openmp'
-OMP_TEST_DIR = CHIMES_HOME + '/src/tests/frontend/openmp'
+OMP_TEST_DIR = CHIMES_HOME + '/src/tests/frontend/' + \
+               get_platform_directory() + '/openmp'
 OMP_H = find_file('omp.h', '/usr/')
 
 SIMPLE_TESTS = ['basic_parallel.cpp', 'nested_parallel.cpp']
