9de2129167a92714a46a96382f93a3785fff7215
Max Grossman <jmaxg3@gmail.com>
2015-05-16 17:34:57 -0500

    fix gitignore

---
+++ src/tests/perf/cpp/compile_sw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+GXX=/opt/apps/gcc/4.8.2/bin/g++ \
+# GXX=/usr/local/bin/g++ \
+    ${CHIMES_HOME}/src/preprocessing/compile_cpp.sh -s -d -k \
+    -i ${CHIMES_HOME}/src/examples/cpp/smithWaterman/smith_waterman.cpp \
+    -o sw_chimes
+
+GXX=/opt/apps/gcc/4.8.2/bin/g++ \
+# GXX=/usr/local/bin/g++ \
+    ${CHIMES_HOME}/src/preprocessing/compile_normal.sh \
+    -i ${CHIMES_HOME}/src/examples/cpp/smithWaterman/smith_waterman.cpp \
+    -o sw_normal
+
---
+++ src/tests/perf/cpp/run_sw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+time ./sw_chimes 55000 220000
+time ./sw_normal 55000 220000
