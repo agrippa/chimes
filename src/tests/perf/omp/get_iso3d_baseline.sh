fb8f5a0f7efab3202f7c7edd0b183bed096607f0
Max Grossman <jmaxg3@gmail.com>
2015-05-26 19:43:34 -0500

    move omp files into dedicated test folder, broken omp lulesh example?

--- src/tests/perf/omp/get_iso3d_baseline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}; do
-    echo Run $i
-    LD_LIBRARY_PATH=$CHIMES_HOME/src/examples/cpp/lib/:$LD_LIBRARY_PATH \
-    # DYLD_LIBRARY_PATH=$CHIMES_HOME/src/examples/cpp/lib/:$DYLD_LIBRARY_PATH \
-                    ./iso3d_normal -x 1536 -y 1024 -z 1024 -i 10
-done &> baseline.log
