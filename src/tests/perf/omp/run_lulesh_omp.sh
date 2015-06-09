f5eb772f3b0633dab64c87de18506c1975f998b9
Max Grossman <jmaxg3@gmail.com>
2015-06-09 18:24:54 -0500

    fix run script

--- src/tests/perf/omp/run_lulesh_omp.sh
+++ src/tests/perf/omp/run_lulesh_omp.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# time ./lulesh_chimes 380
-# time ./lulesh_normal 380
-
-time ./lulesh_normal_omp 2 &> log.normal
-time ./lulesh_chimes_omp 2 &> log.omp
+time ./lulesh_chimes 380
+time ./lulesh_normal 380
