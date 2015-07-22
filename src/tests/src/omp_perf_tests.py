ef26936e7b7ee91ad848dbe3122dd1bd5bda2fc3
Max Grossman <jmaxg3@gmail.com>
2015-07-22 15:52:24 -0500

    update tests so that different machines can run different configurations

--- src/tests/src/omp_perf_tests.py
+++ src/tests/src/omp_perf_tests.py
@@ -1,11 +1,15 @@
 import os
 import sys
+import importlib
 
 from common import PerfTest, RuntimeTest, parse_argv, CHIMES_HOME, \
-         run_runtime_test, run_perf_test, \
+         run_runtime_test, run_perf_test, get_num_cores, \
          is_rodinia_supported, is_spec_supported, get_machine_name
-from shared_tests import MISC_OMP_RUNTIME_TESTS, PERF_CLI_ARGS
+from shared_tests import MISC_OMP_RUNTIME_TESTS
 from exec_time_parsers import EXEC_TIME_PARSERS
+
+perf_cli_args_classname = get_machine_name().replace('.', '_') + '_perf_args'
+perf_cli_args_mod = importlib.import_module(perf_cli_args_classname)
 
 CPP_EXAMPLES_DIR = CHIMES_HOME + '/src/examples/cpp'
 COMPILE_SCRIPT = CHIMES_HOME + '/src/preprocessing/compile_cpp.sh'
@@ -17,19 +21,22 @@ print('Using ' + str(get_num_cores()) + ' core(s)')
 TESTS = []
 
 for test in MISC_OMP_RUNTIME_TESTS:
-    TESTS.append(PerfTest(test, PERF_CLI_ARGS[test.name], EXEC_TIME_PARSERS[test.name]))
+    TESTS.append(PerfTest(test, perf_cli_args_mod.PERF_CLI_ARGS[test.name],
+                          EXEC_TIME_PARSERS[test.name]))
 
 if is_rodinia_supported():
     from rodinia_tests import ALL_RODINIA_RUNTIME_TESTS
     for test in ALL_RODINIA_RUNTIME_TESTS:
-        new_test = PerfTest(test, PERF_CLI_ARGS[test.name], EXEC_TIME_PARSERS[test.name])
+        new_test = PerfTest(test, perf_cli_args_mod.PERF_CLI_ARGS[test.name],
+                            EXEC_TIME_PARSERS[test.name])
         new_test.extra_compile_args += ' -D OPEN'
         TESTS.append(new_test)
 
 if is_spec_supported():
     from spec_tests import ALL_SPEC_RUNTIME_TESTS
     for test in ALL_SPEC_RUNTIME_TESTS:
-        new_test = PerfTest(test, PERF_CLI_ARGS[test.name], EXEC_TIME_PARSERS[test.name])
+        new_test = PerfTest(test, perf_cli_args_mod.PERF_CLI_ARGS[test.name],
+                            EXEC_TIME_PARSERS[test.name])
         new_test.extra_compile_args += ' -D SPEC_OMP -D SPEC_OPENMP '
         TESTS.append(new_test)
 
