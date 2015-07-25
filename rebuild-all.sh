8ba015b2f81b3c5e9094188c9f8b42cf1851a0f9
Max Grossman <jmaxg3@gmail.com>
2015-07-25 18:47:32 -0500

    some script and build file cleanup

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [[ ! $(type nvcc &> /dev/null) ]] ; then
-    cd src/libchimes && make clean && make libchimes_cpp.so -j &
+    cd src/libchimes && make clean && make libchimes_cpp.so libchimes_dummy.so -j &
 else
-    cd src/libchimes && make clean && make -j &
+    cd src/libchimes && make clean && make libchimes_cpp.so libchimes.so libchimes_dummy.so -j &
 fi
 
 cd src/preprocessing/brace_insert && make clean && make -j &
