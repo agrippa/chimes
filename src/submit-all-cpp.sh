2452e532485e0395aa65d2b3d2bc4d039d751b56
Max Grossman <jmaxg3@gmail.com>
2015-07-26 11:25:10 -0500

    merge submit all scripts

--- src/submit-all-cpp.sh
+++
@@ -1,6 +0,0 @@
-for t in $(python  $CHIMES_HOME/src/tests/src/cpp_perf_tests.py -q -t lkjasdf | awk '{ print $1 }'); do
-    echo $t
-    $CHIMES_HOME/src/submit-job.sh $t cpp 00:30:00 2 dummy noblock;
-    $CHIMES_HOME/src/submit-job.sh $t cpp 00:30:00 2 nodummy block;
-    $CHIMES_HOME/src/submit-job.sh $t cpp 00:30:00 2 nodummy noblock;
-done
